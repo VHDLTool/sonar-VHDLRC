d72d3c0f0bf0c021105339f5ade2bdbd39906eaa
Maxime Facquet <facquet.maxime@gmail.com>
2018-07-04 15:10:57 +0200

    Initial commit

---
+++ sonar-vhdlrc-plugin/src/main/java/com/linty/sonar/App.java
@@ -0,0 +1,13 @@
+package com.linty.sonar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ sonar-vhdlrc-plugin/src/main/java/com/linty/sonar/plugins/vhdlrc/VHDLRcPlugin.java
@@ -0,0 +1,33 @@
+package com.linty.sonar.plugins.vhdlrc;
+
+import java.util.Arrays;
+
+import org.sonar.api.Plugin;
+import org.sonar.api.utils.Version;
+
+import com.google.common.collect.ImmutableList;
+
+
+public class VHDLRcPlugin implements Plugin {
+
+ private static final Version SQ_6_7 = Version.create(6, 7);
+	
+ @Override 
+ public void define(Context context) {
+	  ImmutableList.Builder<Object> builder = ImmutableList.builder();
+	    if (!context.getSonarQubeVersion().isGreaterThanOrEqual(SQ_6_7)) {
+	      throw new IllegalStateException("SonarQube 6.7 is required for VHDL plugin");
+	    }
+	    builder.add(
+	    		
+	    		);
+//    context.addExtensions(VHDLToolConfiguration.getPropertyDefinitions());
+//    context.addExtensions(Arrays.asList(
+//            VHDLToolProfile.class,
+//            VHDLToolSensor.class,
+//            VHDLToolConfiguration.class,
+//            Vhdl.class,
+//            STDRulesDefinition.class));
+	  context.addExtensions(builder.build());
+  }
+}
