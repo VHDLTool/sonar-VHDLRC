5a964d62fdf3d356aeadc7d640308421eeb8f3b4
Maxime Facquet <facquet.maxime@gmail.com>
2018-07-11 15:46:19 +0200

    Handle handbook file exceptions

--- sonar-vhdlrc-plugin/src/main/java/com/linty/sonar/plugins/vhdlrc/rules/HandbookXmlParser.java
+++ sonar-vhdlrc-plugin/src/main/java/com/linty/sonar/plugins/vhdlrc/rules/HandbookXmlParser.java
@@ -15,52 +15,65 @@ import javax.xml.namespace.QName;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import java.io.File;
-import java.io.IOException;
-import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class HandbookXmlParser {
 	
 	private static final Logger LOG = Loggers.get(HandbookXmlParser.class);
 	private static final String NAMESPACE_HANDBOOX = "HANDBOOK";
-	
-	private List<Rule> rules;
+	private static final List<Rule> NULL = null;
 	
 	private static boolean fileExists(@Nullable File file) {
 		return file != null && file.exists() && file.isFile();
 	}
 	
-	public void parseXML(File file) {
+	public List<Rule> parseXML(File file) {
 		try {
 			if (fileExists(file)) {
 				if (file.length() == 0) {
 					LOG.warn("File {} is empty and won't be analyzed.", file.getPath());
+					return NULL;
 				}
 				else {
-					SMInputFactory xmlFactory = new SMInputFactory(XMLInputFactory.newInstance());
-					SMInputCursor cursor = xmlFactory.rootElementCursor(file).advance();
-					collectRules(cursor);					
-					LOG.info("Parsing " + file.getName());					
+					List<Rule> rules = new ArrayList<>();
+					collectRules(file,rules);					
+					LOG.info("Parsing {}", file.getPath());
+					return rules;
 				}
 			}
-			else LOG.error("File was not found");
-
+			else if(file==null)
+				throw new NullPointerException();			
+			else if(!file.exists() || !file.isFile()) {
+				LOG.warn("File {} was not found or is not a file and won't be analysed", file.getPath());
+				return NULL;
+			}
+			
+		} catch (NullPointerException e) {
+			LOG.error("Null argument in parseXML()");
+			if (LOG.isDebugEnabled()) {
+				LOG.debug(ExceptionUtils.getFullStackTrace(e));
+			}
+			throw new IllegalStateException(e);
 		} catch ( XMLStreamException e) {
-			LOG.error("Unable to parse "+file.getName(), e);
+			LOG.error("Unable to parse xml file: {}",file.getPath());
 			if (LOG.isDebugEnabled()) {
 				LOG.debug("XML file parsing failed because of : {}", ExceptionUtils.getFullStackTrace(e));
 			}
+			throw new IllegalStateException(e);
 		}
+		return NULL;
 	}
 
-	private void collectRules(SMInputCursor cursor) throws XMLStreamException {
+	private void collectRules(File file, List<Rule> rules) throws XMLStreamException {
+		SMInputFactory xmlFactory = new SMInputFactory(XMLInputFactory.newInstance());
+		SMInputCursor cursor = xmlFactory.rootElementCursor(file).advance();
 		SMInputCursor ruleCursor = cursor.childElementCursor(new QName(NAMESPACE_HANDBOOX, "Rule")).advance();
-		
+		Rule r = new Rule();
+		rules.add(r);		
 	}
 
-	public List<Rule> getRules(){
-		return rules;
-	}
+
 	
 	
 	
--- sonar-vhdlrc-plugin/src/test/java/com/linty/sonar/plugins/vhdlrc/rules/HandbookXmlParserTest.java
+++ sonar-vhdlrc-plugin/src/test/java/com/linty/sonar/plugins/vhdlrc/rules/HandbookXmlParserTest.java
@@ -2,24 +2,92 @@ package com.linty.sonar.plugins.vhdlrc.rules;
 
 import java.io.File;
 import com.linty.sonar.plugins.vhdlrc.rules.HandbookXmlParser;
+
+import org.sonar.api.internal.apachecommons.io.FilenameUtils;
+import org.sonar.api.utils.log.LogTester;
+import org.sonar.api.utils.log.LoggerLevel;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Rule;
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class HandbookXmlParserTest {
 	
+	private HandbookXmlParser XmlParser = new HandbookXmlParser();
+	List<com.linty.sonar.plugins.vhdlrc.rules.Rule> rl1;
+	
+	@Rule
+	public LogTester logTester = new LogTester();
+	
+	@Before
+	public void setup() {
+		File hb = new File("src/test/files/handbooks/VHDL_Handbook_STD-master/Rulesets/handbook_STD.xml");		
+		rl1 = XmlParser.parseXML(hb);		
+	}
 
-	private static final String hbPath="src/test/files/handbooks/VHDL_Handbook_STD-master/Rulesets/handbook_STD.xml";
 	
 	@Test
-	public void Test() {
-		File hb = new File(hbPath);
-		HandbookXmlParser p = new HandbookXmlParser();
-		p.parseXML(hb);
-		//assertThat(p.getRules().get(0).name).isEqualTo(" ");
-		assertThat(p.getRules()).isNull();
-
+	public void Test() {		
+		assertThat(rl1).isNotNull();
+		assertThat(logTester.logs(LoggerLevel.ERROR)).isEmpty();
+		assertThat(logTester.logs(LoggerLevel.WARN)).isEmpty();
 	}
+	
+	//Existing but empty file should return a null List<Rule> and raise a warning
+	@Test
+	public void empty_xml_file_test_should_log_warning() {
+		rl1 = XmlParser.parseXML(new File("src/test/files/handbooks/empty_file.xml"));
+		assertThat(logTester.logs(LoggerLevel.ERROR)).isEmpty();
+		String filename = FilenameUtils.separatorsToSystem("src/test/files/handbooks/empty_file.xml");
+		assertThat(logTester.logs(LoggerLevel.WARN)).contains("File " + filename + " is empty and won't be analyzed.");
+	}
+	
+	//File not found should return a null List<Rule> and raise a warning,
+	//FileNotFoundException is not raised in case multiple handbook would be supported
+	@Test 
+	public void not_existing_file_should_log_error() {
+		rl1 = XmlParser.parseXML(new File("src/test/files/handbooks/non_existing_file.xml"));
+		String filename = FilenameUtils.separatorsToSystem("src/test/files/handbooks/non_existing_file.xml");
+		assertThat(logTester.logs(LoggerLevel.WARN)).contains("File " + filename + " was not found or is not a file and won't be analysed");
+		assertThat(logTester.logs(LoggerLevel.ERROR)).isEmpty();
+	}
+	
+	//
+	@Test
+	public void file_is_a_directory_log_treated_has_not_found() {
+		rl1 = XmlParser.parseXML(new File("src/test/files/handbooks/"));
+		String filename = "src\\test\\files\\handbooks";
+		assertThat(logTester.logs(LoggerLevel.WARN)).contains("File " + filename + " was not found or is not a file and won't be analysed");
+		assertThat(logTester.logs(LoggerLevel.ERROR)).isEmpty();
+	}
+	
+	//Null argument is not supposed to append
+	@Test (expected = IllegalStateException.class)
+	public void null_argument() {
+		File nullFile = null;
+		rl1 = XmlParser.parseXML(nullFile);
+		assertThat(logTester.logs(LoggerLevel.ERROR)).containsExactly("Null argument in parseXML()");
+	}
+	
+	
+		
+	
+	
+	
+	//Existing file with parsing error should raise an error
+//	@Test
+//	  public void should_return_null_when_encountering_parsing_issue() {
+//	    logTester.setLevel(LoggerLevel.DEBUG);
+//	    assertThat(XmlParser.parseXML(new File("src/test/files/handbooks/parsing-issue.xml"))).isNull();
+//	    String filename = FilenameUtils.separatorsToSystem("src/test/files/handbooks/parsing-issue.xml");
+//	    assertThat(logTester.logs(LoggerLevel.ERROR)).contains("Unable to parse xml file: " + filename);
+//	    assertThat(logTester.logs(LoggerLevel.DEBUG)).hasSize(1).contains("XML file parsing failed because of : org.xml.sax.SAXParseException; systemId: file: ");
+//	}
+	
+	
 	
 	
 
