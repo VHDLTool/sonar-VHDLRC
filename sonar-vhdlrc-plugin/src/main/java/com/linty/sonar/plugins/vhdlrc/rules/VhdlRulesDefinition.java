6131020b3011d88b022b2b531057e8e216059b15
Maxime Facquet <facquet.maxime@gmail.com>
2018-07-13 19:36:27 +0200

    Configure RuleDefinition Test

--- sonar-vhdlrc-plugin/src/main/java/com/linty/sonar/plugins/vhdlrc/rules/VhdlRulesDefinition.java
+++ sonar-vhdlrc-plugin/src/main/java/com/linty/sonar/plugins/vhdlrc/rules/VhdlRulesDefinition.java
@@ -1,5 +1,6 @@
 package com.linty.sonar.plugins.vhdlrc.rules;
 
+import org.sonar.api.config.Configuration;
 import org.sonar.api.platform.ServerFileSystem;
 import org.sonar.api.server.ServerSide;
 import org.sonar.api.server.rule.RulesDefinition;
@@ -14,17 +15,17 @@ public class VhdlRulesDefinition implements RulesDefinition {
     public static final String HANDBOOK_PATH_DESC = "Path to the handbook directory. The path may be absolute or relative to the SonarQube server base directory.";
 	
     private final ServerFileSystem serverFileSystem;
+    private final Configuration configuration;
 	
-	public VhdlRulesDefinition (ServerFileSystem serverFileSystem) {
+	public VhdlRulesDefinition (Configuration configuration,ServerFileSystem serverFileSystem) {
+		this.configuration = configuration;
 		this.serverFileSystem = serverFileSystem;
 	}
 	
 	@Override
 	public void define(Context context) {
 		
-		System.out.println(serverFileSystem.getHomeDir().getPath());
-		System.out.println(serverFileSystem.getHomeDir().getAbsolutePath());
-		//System.out.println(serverFileSystem.getHomeDir().getCanonicalPath());
+		
 	}
 
 }
--- sonar-vhdlrc-plugin/src/test/java/com/linty/sonar/plugins/vhdlrc/rules/VhdlRulesDefinitiontest.java
+++ sonar-vhdlrc-plugin/src/test/java/com/linty/sonar/plugins/vhdlrc/rules/VhdlRulesDefinitiontest.java
@@ -2,19 +2,34 @@ package com.linty.sonar.plugins.vhdlrc.rules;
 
 import java.io.File;
 import org.junit.Test;
+import org.sonar.api.config.internal.MapSettings;
 import org.sonar.api.server.rule.RulesDefinition;
 import com.linty.sonar.plugins.vhdlrc.utils.ServerFileSystemTester;
-
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class VhdlRulesDefinitiontest {
+	
+	MapSettings settings = new MapSettings();
 	
 	
 	@Test
 	  public void test() {
 		File ServerHome = new File("src");
-	    VhdlRulesDefinition definition = new VhdlRulesDefinition(new ServerFileSystemTester(ServerHome));
+		settings.setProperty(VhdlRulesDefinition.HANDBOOK_PATH_KEY, "test/files/handbooks");
+		
+	    VhdlRulesDefinition definition = new VhdlRulesDefinition(settings.asConfig(),new ServerFileSystemTester(ServerHome));
 	    RulesDefinition.Context context = new RulesDefinition.Context();
 	    definition.define(context);
+	    
+	    RulesDefinition.Repository repository = context.repository("vhdl-repository");
+	    
+	    assertThat(repository).isNotNull();
+	    assertThat(repository.name()).isEqualTo("VhdlRulecker");
+	    assertThat(repository.language()).isEqualTo("vhdl");
+	    assertThat(repository.rules()).hasSize(74);
+
+	    assertThat(repository.rules()).isNotNull().isNotEmpty();
+	    
 	    
 	}
 
