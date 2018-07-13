83d22c1a82fbc04cf8575b50394acb1ee631451a
Maxime Facquet <facquet.maxime@gmail.com>
2018-07-13 11:24:18 +0200

    Create Rules Definition Extension

---
+++ sonar-vhdlrc-plugin/src/main/java/com/linty/sonar/plugins/vhdlrc/rules/VhdlRulesDefinition.java
@@ -0,0 +1,13 @@
+package com.linty.sonar.plugins.vhdlrc.rules;
+
+import org.sonar.api.server.rule.RulesDefinition;
+import org.sonar.api.server.rule.RulesDefinition.Context;
+
+public class VhdlRulesDefinition implements RulesDefinition {
+	
+	@Override
+	public void define(Context context) {
+		
+	}
+
+}
