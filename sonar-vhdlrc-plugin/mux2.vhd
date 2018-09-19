4811494a0950d86d3d6dc9e0f20a1d6f0fbebb7b
Maxime <facquet.maxime@gmail.com>
2018-09-19 18:59:12 +0700

    ADD HEADER TO FILES
    success, LGPL

--- sonar-vhdlrc-plugin/mux2.vhd
+++
@@ -1,1 +0,0 @@
-an other line
--- sonar-vhdlrc-plugin/src/main/java/com/linty/sonar/plugins/vhdlrc/rules/HandbookXmlParser.java
+++ sonar-vhdlrc-plugin/src/main/java/com/linty/sonar/plugins/vhdlrc/rules/HandbookXmlParser.java
@@ -1,3 +1,20 @@
+/*
+ * Vhdl RuleChecker (Vhdl-rc) plugin for Sonarqube & Zamiacad
+ * Copyright (C) 2018 Maxime Facquet
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package com.linty.sonar.plugins.vhdlrc.rules;
 
@@ -70,6 +87,7 @@ public class HandbookXmlParser {
 			}
 			
 		} catch (XMLStreamException e) {
+		  
 			LOG.error("Error when parsing xml file: {} at line: {}\n{}",file.getPath(),e.getLocation().getLineNumber(),e.getMessage());
 			if (LOG.isDebugEnabled()) {
 				LOG.debug("XML file parsing failed because of :{}",Arrays.toString(e.getStackTrace()));
