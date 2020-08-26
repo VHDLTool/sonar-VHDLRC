a7397d8e46094bcfd21e14e07f4ac92d214c6b05
mroca1 <mathias.roca@linty-services.com>
2020-08-26 20:41:23 +0200

    Add automatic generation of default quality profile from handbook.xml

--- sonar-vhdlrc-plugin/src/main/java/com/linty/sonar/plugins/vhdlrc/VhdlRcProfile.java
+++ sonar-vhdlrc-plugin/src/main/java/com/linty/sonar/plugins/vhdlrc/VhdlRcProfile.java
@@ -19,17 +19,63 @@
 package com.linty.sonar.plugins.vhdlrc;
 
 
+import java.io.File;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+
+import org.codehaus.staxmate.SMInputFactory;
+import org.codehaus.staxmate.in.SMHierarchicCursor;
+import org.codehaus.staxmate.in.SMInputCursor;
 import org.sonar.api.server.profile.BuiltInQualityProfilesDefinition;
+
+import com.linty.sonar.plugins.vhdlrc.rules.VhdlRulesDefinition;
 
 public class VhdlRcProfile implements BuiltInQualityProfilesDefinition{
 	
 	public static final String BUILT_IN_PROFILE_NAME ="VHDL-RC";
+	private NewBuiltInQualityProfile vhdlRcQP;
 
 	@Override
 	public void define(Context context) {
-		NewBuiltInQualityProfile vhdlRcQP = context.createBuiltInQualityProfile(BUILT_IN_PROFILE_NAME, Vhdl.KEY);
-		vhdlRcQP.setDefault(true)
-		.done();
+		vhdlRcQP = context.createBuiltInQualityProfile(BUILT_IN_PROFILE_NAME, Vhdl.KEY);
+        InputStream handbook=getClass().getClassLoader().getResourceAsStream("configuration/HANDBOOK/Rulesets/handbook.xml");		
+	    try {
+	        SMInputFactory inputFactory = initStax();
+	        SMHierarchicCursor rootCursor = inputFactory.rootElementCursor(handbook);
+	        while (rootCursor.getNext() != null) {
+	            addActiveRules(rootCursor.descendantElementCursor("hb:Rule"));
+	          }	       
+	        rootCursor.getStreamReader().closeCompletely();
+	      }
+	      catch (XMLStreamException e) {
+	        throw new IllegalStateException("XML is not valid", e);
+	      }
+		vhdlRcQP.setDefault(true).done();
 		
 	}
+	
+	  private void addActiveRules(SMInputCursor rule) throws XMLStreamException {
+		    while (rule.getNext() != null) {
+		    	String ruleID = rule.getAttrValue("UID");
+		    	SMInputCursor ruleHist = rule.descendantElementCursor("hb:RuleHist");
+			      if (ruleHist.getNext()!=null) {
+			    	  SMInputCursor ruleStatus = ruleHist.descendantElementCursor("hb:Status");
+			    	  if (ruleStatus.getNext()!=null && ruleStatus.getElemStringValue().equalsIgnoreCase("Active"))
+			    		  vhdlRcQP.activateRule(VhdlRulesDefinition.VHDLRC_REPOSITORY_KEY, ruleID);
+			      }
+		    }
+	  }
+		  	
+	
+	 private static SMInputFactory initStax() {
+		    final XMLInputFactory xmlFactory = XMLInputFactory.newInstance();
+		    xmlFactory.setProperty(XMLInputFactory.IS_COALESCING, Boolean.TRUE);
+		    xmlFactory.setProperty(XMLInputFactory.IS_NAMESPACE_AWARE, Boolean.FALSE);
+		    xmlFactory.setProperty(XMLInputFactory.SUPPORT_DTD, Boolean.FALSE);
+		    xmlFactory.setProperty(XMLInputFactory.IS_VALIDATING, Boolean.FALSE);
+		    return new SMInputFactory(xmlFactory);
+		  }
 }
