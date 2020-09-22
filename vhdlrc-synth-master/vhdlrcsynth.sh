1c0ddf53bab8b5f2992bd598810bf111f135d07d
mroca1 <33022454+mroca1@users.noreply.github.com>
2020-09-22 10:59:16 +0200

    Update vhdlrcsynth.sh

--- vhdlrc-synth-master/vhdlrcsynth.sh
+++ vhdlrc-synth-master/vhdlrcsynth.sh
@@ -1,6 +1,7 @@
 files_regex=".*\.\(vhdl\|vhd\)"
 
 ghdl --clean
+# Analyze the list of files passed as parameters. If invoked with no list of files, all files with vhd or vhdl extension in the project will be analyzed.
 if [ -z $2 ]
 	then
 	ghdl -a `find ./ -regex $files_regex| tr '\n' ' '`
