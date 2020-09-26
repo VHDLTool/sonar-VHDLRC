422c4b3eb6878f20be0ecd51d33203ee9faa5382
mroca1 <mathias.roca@linty-services.com>
2020-09-26 02:44:55 +0200

    Update vhdlrcsynth.sh

--- vhdlrc-synth-master/vhdlrcsynth.sh
+++ vhdlrc-synth-master/vhdlrcsynth.sh
@@ -2,13 +2,11 @@ files_regex=".*\.\(vhdl\|vhd\)"
 
 ghdl --clean
 # Analyze the list of files passed as parameters. If invoked with no list of files, all files with vhd or vhdl extension in the project will be analyzed.
-if [ -z $2 ]
+if [ -z $3 ]
 	then
-	ghdl -a `find ./ -regex $files_regex| tr '\n' ' '`
+	ghdl -a $2 `find ./ -regex $files_regex| tr '\n' ' '`
 	else
-	files=${@:2}
-	ghdl -a $files
+	files=${@:3}
+	ghdl -a $2 $files
 	fi
-ghdl -e $1
-
-yosys -m ghdl -p "ghdl; setattr -set fsm_encoding \"auto\"; fsm -norecode -nomap -export"
+ghdl -e $2 $1
